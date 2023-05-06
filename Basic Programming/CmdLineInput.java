bcd3e65fd77b56dd2517729f9e9cdf6d9f128559
Aviral Srivastava <96352803+KailWensen@users.noreply.github.com>
2023-05-06 22:28:37 +0530

    Add files via upload

---
+++ Basic Programming/CmdLineInput.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class CmdLineInput
+{
+	public static void main(String args[])
+	{
+	System.out.println("The First input is: "+ args[0]);	
+	System.out.println("The Second input is: "+ args[1]);
+	}
+}
+//run through cmd prompt. after compilling, run the interpreter and add the input after the name of the program.
+// javac CmdLineInput.java
+//java CmdLineInput Aviral Srivastava //here "Aviral Srivastava is the input.
