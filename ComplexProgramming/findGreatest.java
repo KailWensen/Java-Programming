9fa64c9deccca254503c808cb25d0156b5a4834b
Aviral Srivastava <96352803+KailWensen@users.noreply.github.com>
2023-05-10 01:21:29 +0530

    Add files via upload

---
+++ ComplexProgramming/findGreatest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class findGreatest 
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter any 3 numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a>b && a>c)
+        { System.out.println(a+ "is the greatest number");}
+
+        else if (b>a && b>c)
+        {System.out.println(b+" is the greatest number");}
+
+        else 
+        { System.out.println(c+ " is the greatest number");}
+
+    }
+    
+}
