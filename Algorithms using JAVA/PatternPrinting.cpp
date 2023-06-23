7b8e84c0b4d418530c279cd621b0a0bfa33fa762
Aviral Srivastava <96352803+KailWensen@users.noreply.github.com>
2023-06-23 23:51:57 +0530

    Add files via upload

---
+++ Algorithms using JAVA/PatternPrinting.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printDiamond(int n)
+{
+    int space = n - 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < space; j++)
+            cout << " ";
+
+        for (int j = 0; j <= i; j++)
+            cout << "* ";
+
+        cout << endl;
+        space--;
+    }
+    space = 0;
+
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < space; j++)
+            cout << " ";
+
+        for (int j = 0; j < i; j++)
+            cout << "* ";
+
+        cout << endl;
+        space++;
+    }
+}
+
+int main()
+{
+    int numberOfRows;
+    cout<<"Enter the number of rows: "<<endl;
+    cin>>numberOfRows;
+    printDiamond(numberOfRows);
+    return 0;
+}
