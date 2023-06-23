1d4b09cce442cfb369ef848069b6abadb34c8937
Aviral Srivastava <96352803+KailWensen@users.noreply.github.com>
2023-06-23 23:46:41 +0530

    Add files via upload

---
+++ Algorithms using JAVA/WordCalc.py
@@ -0,0 +1,14 @@
+from word2number import w2n
+  
+# initializing string
+test_str = "zero four zero one"
+  
+# printing original string
+print("The original string is : " + test_str)
+  
+# Convert numeric words to numbers
+# Using word2number
+res = w2n.word_to_num(test_str)
+  
+# printing result 
+print("The string after performing replace : " + str(res)) 
